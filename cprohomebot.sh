a43e7abaa4fa6af3d281de321fed857151bbb899
Joshua <family@laruecrew.ca>
2017-05-04 19:00:04 -0600

    Create cprohomebot.sh

---
+++ cprohomebot.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "This first part of the script will set up your CHIP Pro to be able to download and run the HomeBot script."
+
+echo "This script will ask for your password."
+
+#Installs the set_antenna script
+echo "Installing set_antenna script..."
+curl -O https://raw.githubusercontent.com/NextThingCo/CHIP-buildroot/d4e5ddd02b5b5cb3ae37026b0743dcba1c724fc3/package/rtl8723ds_mp_driver/set_antenna
+chmod +x set_antenna
+sudo mv set_antenna /usr/sbin
+sudo set_antenna ufl
+echo "Done!"
+sed -i -e '$i \set_antenna ufl\n' /etc/rc.local
+echo "Done!"
+
+#Updates operating system
+echo "Updating OS..."
+sudo apt-get update
+sudo apt-get upgrade
+echo "Done!"
+
+#Installs Python
+echo "Installing Python..."
+sudo apt-get install python
+echo "Done!"
+
+#Installs the unnoficial CHIP_IO v5.5 package
+echo "Installing CHIP_IO..."
+curl -O https://github.com/xtacocorex/CHIP_IO/releases/download/v0.5.6/python-chip-io_0.5.6-1_armhf.deb
+sudo dpkg -i python-chip-io_0.5.6-1_armhf.deb
+echo "Done!"
+
+#Starts HomeBot script
+echo "Installing HomeBotOS..."
+curl -o homebot.py https://raw.githubusercontent.com/Jalonzpa/homebot_code/master/homebot.py
+chmod +x homebot.py
+echo "Installed!"
+echo "Booting HomeBot..."
+python homebot.py
