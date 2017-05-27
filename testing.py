9c967d5b7acef7552b2a45a7e503475efdfd7974
Joshua <family@laruecrew.ca>
2017-05-27 09:38:39 -0600

    Update
    Structure change

--- testing.py
+++ testing.py
@@ -5,11 +5,8 @@ allgood=False
 allgood2=False
 def talk(my_string):
     os.system('/usr/bin/flite -t "' + my_string + '"')
-
-talk("Hello! I am HomeBot, your personal cooking helper. I will help you with all your cooking needs, help set reminders, and also helping you text people while you are cooking so you don't get your phone  dirty.")
-talk("So, first I need to ask you some questions.")
-
-while not allgood:
+    
+def questions():
     talk("First, What is your favorite meal? Choose from chicken, kraft dinner, or potatoes.")
     favmeal=raw_input()
     talk("Second, how often do you have that meal?")
@@ -18,7 +15,7 @@ while not allgood:
     famlike=raw_input()
     talk("So you have %r %r, and %r." % (favmeal, freq, famlike.replace("yes", "everyone in your family likes %s") % (favmeal)))
 
-
+def favmealcheck():
     if favmeal == 'chicken':
         talk("Your favorite meal is chicken, correct?")
         allgood=True
@@ -31,14 +28,9 @@ while not allgood:
     else:
         talk("You didn't write either chicken, kraft dinner, or potatoes. Please try again.")
         allgood=False
-
-talk("How many times do you want me to help you make %s this week? (Answer with a number.)" % favmeal)
-timeweek=raw_input()
-time.sleep(1)
-talk("Ok, I'll help you make %s %s times this week." % (favmeal, freq))
-
-while not allgood2:
-    if favmeal == 'chicken':
+        
+def ingredients():
+        if favmeal == 'chicken':
         talk("You need these ingredients: a whole chicken, herbs like rosemary, chive, cilantro, or anything else, whole lemons, pepper, and salt. Do you have all these ingredients?")
         ingravailable=raw_input()
         time.sleep(1)
@@ -50,10 +42,9 @@ while not allgood2:
         talk("You need these ingredients: potatoes, cheese, sour cream, and bacon. Do you have those?")
         ingravailable=raw_input()
         time.sleep(1)
-
-
-
-    if ingravailable == 'yes':
+        
+def ingrcheck():
+        if ingravailable == 'yes':
         talk("Great! You are all set for tonight.")
         allgood2=True
     elif ingravailable == 'no':
@@ -62,7 +53,8 @@ while not allgood2:
     elif ingravailable == None:
         talk("You need to type something in.")
         allgood2=False
-
+        
+def instructions()
     if favmeal == 'chicken':
         talk("All right. Here's the first thing to do: preheat the oven to 400 degrees.")
         time.sleep(2)
@@ -77,8 +69,7 @@ while not allgood2:
         talk("Once you have it out, you can start to pull it apart. Be careful! It's hot! Make sure to pull along the grain, not against it.")
         time.sleep(2)
         talk("You can now serve it in buns, or eat it by itself.")
-
-    if favmeal == 'kraft dinner':
+    elif favmeal == 'kraft dinner':
         talk("First things first; bring a big pot of water to a boil. Cook 8 ounces of elbow noodles in the water (while it's boiling), making sure to stir until cooked but still firm to the bite, usually 8 minutes.")
         time.sleep(2)
         talk("Drain the noodles now. Melt a quarter cup butter in a pan over medium heat, and stir in a quarter cup all purpose flour and half a teaspoon salt until smooth (usually 5 minutes).")
@@ -90,8 +81,7 @@ while not allgood2:
         talk("Stir the mixture into the noodles until the whole thing is coated.")
         time.sleep(1)
         talk("Now you can add a bit more pepper if you want. You can now serve it!")
-
-    if favmeal == 'potatoes':
+    elif favmeal == 'potatoes':
         talk("Make sure to wash and scrub the potato. Prick it in several places with a fork.")
         time.sleep(1)
         talk("Cook in the microwave for 5 minutes. Then, turn it over and cook for 5 more minutes.")
@@ -101,3 +91,23 @@ while not allgood2:
         talk("Return it to the microwave and cook for about one more minute.")
         time.sleep(1)
         talk("Top it with more cheese, if you'd like, and sour cream, also optional. You can now serve it!")
+        
+
+talk("Hello! I am HomeBot, your personal cooking helper. I will help you with all your cooking needs, help set reminders, and also helping you text people while you are cooking so you don't get your phone  dirty.")
+talk("So, first I need to ask you some questions.")
+
+while not allgood:
+    questions()
+    favmealcheck()
+    
+
+talk("How many times do you want me to help you make %s this week? (Answer with a number.)" % favmeal)
+timeweek=raw_input()
+time.sleep(1)
+talk("Ok, I'll help you make %s %s times this week." % (favmeal, freq))
+
+while not allgood2:
+    ingredients()
+    ingrcheck()
+    
+instructions()
