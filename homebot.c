8ab1d1a7ed59cb1a8cc1ebb3337311ac50f57540
Joshua <family@laruecrew.ca>
2017-08-04 09:09:19 -0600

    Created
    Need to add instructions and add TTS functionality

---
+++ homebot.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+void delay(int delaytime); //time function prototype
+
+void main(void) //main function
+{
+	int if_selection = 0, menu_selection = 0; //declares and initializes two variables
+	bool program_running = true;
+	
+	while (program_running)
+	{	
+		printf("MENU:\n\n1. Chicken\n2. Kraft Dinner\n3. Potatoes\n\n"); //Prints main menu
+		
+		scanf("%i", &menu_selection); //Continually scans for an integer typed by the user
+
+		while (if_selection == 0)
+		{
+			switch (menu_selection)
+			{
+			case 1: //If the user types 1
+				system("cls");
+				printf("Well chicken it is!");
+				delay(2);
+				system("cls");
+
+				//Give step by step recipe instructions for chicken.
+
+
+				break;
+			case 2: //If user types 2
+
+				break;
+
+      case 3: //If user types 3
+      
+        break;
+        
+      default: //If user doesn't type 1, 2, or 3
+				printf("I'm sorry, you didn't enter a correct value. Please try again.");
+
+				delay(2);
+
+				system("cls");
+
+				break;
+			}
+			break;
+		}
+	} //Main While
+
+} //Main
+
+void delay(int delaytime)
+{
+	int Time;
+
+	Time = time(0) + delaytime;
+
+	while (time(0) != Time)
+	{
+
+	}
+
+}
