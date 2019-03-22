0329bbbe08d82a433a69bda9d657a4a18378c597
Jalonzpa <jalonzpa@gmail.com>
2019-03-22 16:32:28 +0000

    ohoho this one's super slick

--- nhb.py
+++ nhb.py
@@ -7,10 +7,15 @@ import config
 allgood=False
 allgood2=False
 allgood3=False
-favmeal=""
+meal=""
 freq=""
 famlike=""
 ingravailable=""
+
+with open("instructions.txt") as f:
+        lines = f.readlines()
+
+lines = [line.rstrip('\n') for line in open("instructions.txt")]
 
 def sprint(s):
     for c in s:
@@ -26,34 +31,34 @@ def main():
 #Asks user baseline questions
     def questions():
         os.system("clear")
-	sprint("So, what do you want tonight? ")
-	sprint("We have a few things on the menu currently.\nType the number that corresponds with the food you want!\n")
-	time.sleep(1)
-	sprint("\n\nMENU:\n\n1. Chicken\n2. Kraft dinner\n3. Potatoes\n")
-        global favmeal
-        menu=raw_input()
-	if menu == "1":
-		favmeal = "chicken"
-	if menu == "2":
-		favmeal = "kraft dinner"
-	if menu == "3":
-		favmeal = "potatoes"
-	sprint("Awesome! I'll help you make %s." % (favmeal))
-	time.sleep(1)
+        sprint("So, what do you want tonight? ")
+        sprint("We have a few things on the menu currently.\nType the number that corresponds with the food you want!\n")
+        time.sleep(1)
+        sprint("\n\nMENU:\n\n1. Chicken\n2. Kraft dinner\n3. Potatoes\n")
+        global meal
+        menu = input()
+        if menu == "1":
+            meal = "chicken"
+        if menu == "2":
+            meal = "kraft dinner"
+        if menu == "3":
+            meal = "potatoes"
+        sprint("Awesome! I'll help you make %s." % (meal))
+        time.sleep(1)
     # Tells the user what ingredients they need
     def ingredients():
-	os.system("clear")
+        os.system("clear")
         global ingravailable
-        if favmeal == 'chicken':
-	    sprint(config.ingredients["chicken"] + " (y/n)")
-            ingravailable=raw_input()
-        elif favmeal == 'kraft dinner':
-	    sprint(config.ingredients["kd"] + " (y/n)")
-            ingravailable=raw_input()
+        if meal == 'chicken':
+            sprint(config.ingredients["chicken"] + " (y/n)")
+            ingravailable=input()
+        elif meal == 'kraft dinner':
+            sprint(config.ingredients["kd"] + " (y/n)")
+            ingravailable=input()
             allgood2=True
-        elif favmeal == 'potatoes':
-	    sprint(config.ingredients["potatoes"] + " (y/n)")
-            ingravailable=raw_input()
+        elif meal == 'potatoes':
+            sprint(config.ingredients["potatoes"] + " (y/n)")
+            ingravailable=input()
             allgood2=True
     # sprints to the user about their ingredients, or lack thereof
     def ingrcheck():
@@ -66,59 +71,34 @@ def main():
             elif ingravailable == 'n':
                 sprint("All right. If you go get those ingredients, I'll help you when you get back. Deal?")
                 allgood3=True
-		sys.exit()
+                sys.exit()
             else:
                 sprint("Whoops! Something went wrong! Exiting...")
-		sys.exit()
+                sys.exit()
     # Gives the user instructions on how to cook their meal
     def instructions():
-        if favmeal == 'chicken':
-            sprint("All right. Here's the first thing to do: preheat the oven to 400 degrees.")
-            time.sleep(2)
-            sprint("Rub the chicken all over with salt and pepper. Then, cut slits in the chicken and tuck your herbs in.")
-            time.sleep(2)
-            sprint("Now, put the diced lemon into the cavity.")
-            time.sleep(2)
-            sprint("Put the chicken in a roasting pan, and put it in the oven with the cover on. Let it cook for one to one and a half hours, and then lower the temperature to 350 degrees.")
-            time.sleep(2)
-            sprint("Keep it in until the internal temperature is 165 degrees.")
-            time.sleep(2)
-            sprint("Once you have it out, you can start to pull it apart. Be careful! It's hot! Make sure to pull along the grain, not against it.")
-            time.sleep(2)
-            sprint("You can now serve it in buns, or eat it by itself.")
-        elif favmeal == 'kraft dinner':
-            sprint("First things first; bring a big pot of water to a boil. Cook 8 ounces of elbow noodles in the water (while it's boiling), making sure to stir until cooked but still firm to the bite, usually 8 minutes or so.")
-            time.sleep(2)
-            sprint("Drain the noodles now. Melt a quarter cup butter in a pan over medium heat, and stir in a quarter cup all purpose flour and half a teaspoon salt until smooth (usually 5 minutes).")
-            time.sleep(2)
-            sprint("Slowly pour 2 cups milk into the butter flour mixture whule continuously stirring until the mix is smooth and bubbling.")
-            time.sleep(2)
-            sprint("Add 2 cups shredded cheddar cheese to the milk mixture and stir until the mix is melted, usually 2 to 4 minutes.")
-            time.sleep(2)
-            sprint("Stir the mixture into the noodles until the whole thing is coated.")
-            time.sleep(1)
-            time.sleep(1)
-            sprint("Now you can add a bit more pepper if you want. You can now serve it!")
-        elif favmeal == 'potatoes':
-            sprint("Make sure to wash and scrub the potato. Prick it in several places with a fork.")
-            time.sleep(1)
-            sprint("Cook in the microwave for 5 minutes. Then, turn it over and cook for 5 more minutes.")
-            time.sleep(2)
-            sprint("Season it with as much pepper and salt as you'd like, and mash up the inside a bit with a fork. Top it with some butter and 2 tablespoons of cheese.")
-            time.sleep(2)
-            sprint("Return it to the microwave and cook for about one more minute.")
-            time.sleep(1)
-            sprint("Top it with more cheese, if you'd like, and sour cream, also optional. You can now serve it!")
-
+        os.system("clear")
+        if meal == "chicken":
+            for i in config.chicken:
+                print(lines[i])
+                input("\nPress [Enter] to continue.\n")
+        if meal == "kraft dinner":
+            for i in config.kd:
+                print(lines[i])
+                input("\nPress [Enter] to continue.\n")
+        if meal == "potatoes":
+            for i in config.potatoes:
+                print(lines[i])
+                input("\nPress [Enter] to continue.\n")
 # Introduction
     sprint("Hello! I am HomeBot, your personal cooking helper.\nI am going to help you with all your cooking needs! ")
     time.sleep(3)
     questions()
 
-    sprint("How many times do you want me to help you make %s this week? (Answer with a number.)\n" % favmeal)
-    timeweek=raw_input()
+    sprint("\n\nHow many times do you want me to help you make %s this week? (Answer with a number.)\n" % meal)
+    timeweek=input()
     time.sleep(1)
-    sprint("\n\nOk, I'll help you make %s %s times this week." % (favmeal, timeweek))
+    sprint("\nOk, I'll help you make %s %s times this week." % (meal, timeweek))
     time.sleep(2)
 
     ingredients()
--- tst.py
+++ tst.py
@@ -6,7 +6,6 @@ os.system("clear")
 
 done = False
 meal = input("What are we having for supper tonight?\n")
-print(meal)
 number = 0
 
 with open("instructions.txt") as f:
@@ -14,21 +13,39 @@ with open("instructions.txt") as f:
 
 lines = [line.rstrip('\n') for line in open("instructions.txt")]
 
-
 if meal == "chicken":
-	while done == False:
-		print(lines[config.chicken[number]])
-		input("\nPress [Enter] to continue.\n")
-		number += 1
-		if number == config.chicken[6]:
-			done = True
+        for i in config.chicken:
+                print(lines[i])
+                input("\nPress [Enter] to continue.\n")
 if meal == "kraft dinner":
-	print(lines[config.kd[number]])
-	input("\nPress [Enter] to continue.\n")
-	number += 1
+	for i in config.kd:
+		print(lines[i])
+		input("\nPress [Enter] to continue.\n")
 if meal == "potatoes":
-	print(lines[config.potatoes[number]])
-	number += 1
+        for i in config.potatoes:
+                print(lines[i])
+                input("\nPress [Enter] to continue.\n")
+#if meal == "chicken":
+#	while done == False:
+#		print(lines[config.chicken[number]])
+#		input("\nPress [Enter] to continue.\n")
+#		number += 1
+#		if number == config.chicken[-1]:
+#			done = True
+#if meal == "kraft dinner":
+#	while done == False:
+#		print(lines[config.kd[number]])
+#		input("\nPress [Enter] to continue.\n")
+#		number += 1
+#		if number == config.kd[-1]:
+#			done = True
+#if meal == "potatoes":
+#	while done == False:
+#		print(lines[config.potatoes[number]])
+#		input("\nPress [Enter] to continue.\n")
+#		number += 1
+#		if number == config.potatoes[-1]:
+#			done = True
 #while True:
 #	print(lines[number])
 #	time.sleep(5)
